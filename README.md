8678db67189f2c62716aa6d68843cdf80b90a9f8
Sengolda <senushkabatawala@gmail.com>
2023-06-07 17:20:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="center">Badgie</h1>
+<p align="center">Badgie is the most simple way to get that sweet sweet discord active developer badge</p>
+
+### Why was this made?
+
+Well about a couple of months ago discord made the [discord active developer badge](https://support-dev.discord.com/hc/en-us/articles/10113997751447-Active-Developer-Badge) and yes, you can read the article and make your own bot and do it. But no one wants to do that so this program makes that whole process alot easier.
+
+
+### How to use
+
+- Make a [discord bot in the discord developer portal here](https://discord.com/developers/applications)
+- Python 3.8 or higher is required [download it here](https://www.python.org/downloads/)
+- Install or clone the repo
+- After installing open the command line and run the following:
+
+```bash
+# MacOS/Linux
+cd badgie
+python3 -m pip install -Ur requirements.txt
+python3 main.py
+
+# Windows
+cd badgie
+py -3 -m pip install -Ur requirements.txt
+python3 main.py
+```
+The rest is done interactively
+
+Please give a star if this project helped you
---
+++ main.py
@@ -0,0 +1,77 @@
+import sys
+import json
+import asyncio
+
+import discord
+from discord import app_commands
+from discord.utils import oauth_url
+
+if not sys.version_info[:2] >= (3, 8):
+    print(
+        "Python 3.8 or higher must be installed to run this program. If it is installed try `python3 main.py`",
+        file=sys.stderr,
+    )
+    sys.exit(1)
+
+config = {}
+try:
+    with open("./config.json") as f:
+        config = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError):
+    pass
+
+token = config.get("token")
+if not token:
+    print("Token is missing please input token")
+    token = input("token:  ")
+    config["token"] = token
+
+
+to_sync_guild = None
+guild_id = config.get("guild_id")
+
+if guild_id is None:
+    guild_id = input(
+    "Input the id of the guild you will be using (this is not required but putting the id would be faster):  "
+    )
+    guild_id = int(guild_id) if guild_id  else 0
+    config["guild_id"] = guild_id
+    to_sync_guild = discord.Object(id=guild_id)
+
+with open("./config.json", "w") as f:
+    json.dump(config, f, indent=4)
+
+class BadgieBadgeGiver(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.tree = app_commands.CommandTree(self)
+
+    async def on_ready(self):
+        print("Badgie Giver is ready!\n\n Use this to invite the bot:  ", oauth_url(self.user.id, permissions=discord.Permissions.all()))
+
+    def run(self):
+        super().run(token)
+
+    async def setup_hook(self):
+        if not to_sync_guild or not to_sync_guild.id:
+            await self.tree.sync()
+        else:
+            self.tree.copy_global_to(guild=to_sync_guild)
+            await self.tree.sync(guild=to_sync_guild)
+
+
+client = BadgieBadgeGiver()
+
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    msg = (
+        f"Hi, {interaction.user.mention},",
+        "please visit https://discord.com/developers/active-developer to get your badge, if you don't see it just wait 24 hours and you will see it.",
+        "if you still do not get it even after that time period please open an issue at: ",
+    )
+
+    await interaction.response.send_message("\n".join(msg))
+
+
+client.run()
